81033e9839dd13b89d6f952e228723052f8c5be5
Emily Richardson <richar35@mail.nmc.edu>
2020-11-10 20:59:19 -0500

    Add project files.

---
+++ EmployeeCh12Main/Models/DepartmentLocation.cs
@@ -0,0 +1,21 @@
+﻿using EmployeesCh12.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeCh12Main.Models
+{
+    public class DepartmentLocation
+    {
+        //Composite primary key and foreign key for department
+        public int DepartmentID { get; set; }
+        //composite Primary key and foreign key for Location
+        public int LocationID { get; set; }
+
+        //navigation properties
+        public Department Department { get; set; }
+        public Location Location { get; set; }
+    }
+    }
+
